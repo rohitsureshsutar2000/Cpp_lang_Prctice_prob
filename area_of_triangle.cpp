9851ffe5b027248038657ec7e72018f7dc616a98
Rohit Sutar <114717436+rohitsureshsutar2000@users.noreply.github.com>
2023-09-16 10:27:42 -0700

    Add files via upload

---
+++ area_of_triangle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int Height,base;
+	cout<<"Enter Heigth and base number:-"<<endl;
+	cin>>Height>>base;
+	
+	cout<<"Area of triangle:- " <<(float) 1/2*(Height*base);
+}
